982b988a5970a7b5cb038f6f4d7ae9dbdcbe7da6
Sangyao42 <wangsangyao@hotmail.com>
2023-02-18 23:50:45 +0100

    Added: main

---
+++ src/push_swap.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sawang <sawang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 23:42:48 by sawang            #+#    #+#             */
+/*   Updated: 2023/02/18 23:50:07 by sawang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	char	**input;
+
+	if (argc < 2)
+		ft_putstr_fd(2, "Error\n");
+	else if (argc == 2)
+		input = ft_split((*argv + 1));
+	else
+		input = argv;
+	if (input_is_valid(argc - 1, input))
+		return (1);
+	// atoi
+	return (0);
+}
